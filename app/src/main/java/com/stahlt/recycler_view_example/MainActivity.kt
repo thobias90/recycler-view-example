2f1f831de22d039db13b9b5145912261d920ec35
Thobias Stahlschmidt <thobias_stahlschmidt@whirlpool.com>
2023-09-16 13:11:01 -0300

    feat: add recycler view backend

--- app/src/main/java/com/stahlt/recycler_view_example/MainActivity.kt
+++ app/src/main/java/com/stahlt/recycler_view_example/MainActivity.kt
@@ -2,10 +2,19 @@ package com.stahlt.recycler_view_example
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import com.stahlt.recycler_view_example.adapter.ItemAdapter
+import com.stahlt.recycler_view_example.data.DataSource
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val itemAdapter = ItemAdapter(this, DataSource().load())
+        recyclerView = findViewById(R.id.recycler_view)
+
+        recyclerView.adapter = itemAdapter
     }
 }
