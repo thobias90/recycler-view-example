3b10646a6a5e3ee15141a5296e91a7b0a4647a6f
Thobias Stahlschmidt <thobias_stahlschmidt@whirlpool.com>
2023-09-16 13:09:59 -0300

    feat: add data source logic

--- app/src/main/java/com/stahlt/recycler_view_example/data/DataSource.kt
+++ app/src/main/java/com/stahlt/recycler_view_example/data/DataSource.kt
@@ -1,4 +1,21 @@
 package com.stahlt.recycler_view_example.data
 
+import com.stahlt.recycler_view_example.R
+import com.stahlt.recycler_view_example.model.Phrase
+
 class DataSource {
+    fun load(): List<Phrase> {
+        return listOf<Phrase>(
+            Phrase(R.string.phrase1),
+            Phrase(R.string.phrase2),
+            Phrase(R.string.phrase3),
+            Phrase(R.string.phrase4),
+            Phrase(R.string.phrase5),
+            Phrase(R.string.phrase6),
+            Phrase(R.string.phrase7),
+            Phrase(R.string.phrase8),
+            Phrase(R.string.phrase9),
+            Phrase(R.string.phrase10)
+        )
+    }
 }
