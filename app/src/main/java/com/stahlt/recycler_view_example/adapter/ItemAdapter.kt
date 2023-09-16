c40fcb9fcc174b3897bb4a5a3250ae4ad02abd98
Thobias Stahlschmidt <thobias_stahlschmidt@whirlpool.com>
2023-09-16 13:09:45 -0300

    feat: implement adapater

--- app/src/main/java/com/stahlt/recycler_view_example/adapter/ItemAdapter.kt
+++ app/src/main/java/com/stahlt/recycler_view_example/adapter/ItemAdapter.kt
@@ -1,4 +1,33 @@
 package com.stahlt.recycler_view_example.adapter
 
-class MyAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.stahlt.recycler_view_example.R
+import com.stahlt.recycler_view_example.model.Phrase
+
+class ItemAdapter(private val context: Context, private val phrases: List<Phrase>): RecyclerView.Adapter<ItemAdapter.ViewHolder>() {
+    class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
+        val tvTitle: TextView
+        init {
+            tvTitle = view.findViewById(R.id.tvTitle)
+        }
+    }
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(viewGroup.context)
+            .inflate(R.layout.list_element, viewGroup, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return phrases.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.tvTitle.text = phrases[position].toString()
+    }
 }
